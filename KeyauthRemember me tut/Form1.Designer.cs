b4c2ff7f72d0cff5d8d558bad3c03e89077b81b9
bectorharbosa <83093441+Lordalpha001@users.noreply.github.com>
2024-09-08 15:30:14 -0400

    Add files via upload

---
+++ KeyauthRemember me tut/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace KeyauthRemember_me_tut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.siticoneElipse1 = new Siticone.UI.WinForms.SiticoneElipse(this.components);
+            this.siticoneMaterialTextBox1 = new Siticone.UI.WinForms.SiticoneMaterialTextBox();
+            this.siticoneMaterialTextBox2 = new Siticone.UI.WinForms.SiticoneMaterialTextBox();
+            this.siticoneOSToggleSwith1 = new Siticone.UI.WinForms.SiticoneOSToggleSwith();
+            this.siticoneLabel2 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneGradientButton1 = new Siticone.UI.WinForms.SiticoneGradientButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.panel1.Controls.Add(this.siticoneLabel1);
+            this.panel1.Location = new System.Drawing.Point(-35, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(636, 46);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel1.ForeColor = System.Drawing.Color.White;
+            this.siticoneLabel1.Location = new System.Drawing.Point(47, 9);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(84, 32);
+            this.siticoneLabel1.TabIndex = 1;
+            this.siticoneLabel1.Text = "example";
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this.panel1;
+            // 
+            // siticoneElipse1
+            // 
+            this.siticoneElipse1.BorderRadius = 7;
+            this.siticoneElipse1.TargetControl = this;
+            // 
+            // siticoneMaterialTextBox1
+            // 
+            this.siticoneMaterialTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneMaterialTextBox1.DefaultText = "";
+            this.siticoneMaterialTextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneMaterialTextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneMaterialTextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox1.DisabledState.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.siticoneMaterialTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox1.FocusedState.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.ForeColor = System.Drawing.Color.White;
+            this.siticoneMaterialTextBox1.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox1.HoveredState.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.Location = new System.Drawing.Point(93, 67);
+            this.siticoneMaterialTextBox1.Name = "siticoneMaterialTextBox1";
+            this.siticoneMaterialTextBox1.PasswordChar = '\0';
+            this.siticoneMaterialTextBox1.PlaceholderText = "";
+            this.siticoneMaterialTextBox1.SelectedText = "";
+            this.siticoneMaterialTextBox1.ShadowDecoration.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.Size = new System.Drawing.Size(200, 36);
+            this.siticoneMaterialTextBox1.TabIndex = 1;
+            // 
+            // siticoneMaterialTextBox2
+            // 
+            this.siticoneMaterialTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneMaterialTextBox2.DefaultText = "";
+            this.siticoneMaterialTextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneMaterialTextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneMaterialTextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox2.DisabledState.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.siticoneMaterialTextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox2.FocusedState.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.ForeColor = System.Drawing.Color.White;
+            this.siticoneMaterialTextBox2.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox2.HoveredState.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.Location = new System.Drawing.Point(93, 109);
+            this.siticoneMaterialTextBox2.Name = "siticoneMaterialTextBox2";
+            this.siticoneMaterialTextBox2.PasswordChar = '\0';
+            this.siticoneMaterialTextBox2.PlaceholderText = "";
+            this.siticoneMaterialTextBox2.SelectedText = "";
+            this.siticoneMaterialTextBox2.ShadowDecoration.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.Size = new System.Drawing.Size(200, 36);
+            this.siticoneMaterialTextBox2.TabIndex = 2;
+            // 
+            // siticoneOSToggleSwith1
+            // 
+            this.siticoneOSToggleSwith1.CheckedFillColor = System.Drawing.Color.White;
+            this.siticoneOSToggleSwith1.CheckedInnerColor = System.Drawing.Color.Gray;
+            this.siticoneOSToggleSwith1.Location = new System.Drawing.Point(93, 192);
+            this.siticoneOSToggleSwith1.Name = "siticoneOSToggleSwith1";
+            this.siticoneOSToggleSwith1.Size = new System.Drawing.Size(34, 18);
+            this.siticoneOSToggleSwith1.TabIndex = 3;
+            this.siticoneOSToggleSwith1.Text = "siticoneOSToggleSwith1";
+            this.siticoneOSToggleSwith1.UncheckedFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.siticoneOSToggleSwith1.UncheckInnerColor = System.Drawing.Color.Gray;
+            // 
+            // siticoneLabel2
+            // 
+            this.siticoneLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel2.ForeColor = System.Drawing.Color.White;
+            this.siticoneLabel2.Location = new System.Drawing.Point(133, 193);
+            this.siticoneLabel2.Name = "siticoneLabel2";
+            this.siticoneLabel2.Size = new System.Drawing.Size(86, 17);
+            this.siticoneLabel2.TabIndex = 2;
+            this.siticoneLabel2.Text = "Remember me?";
+            // 
+            // siticoneGradientButton1
+            // 
+            this.siticoneGradientButton1.CheckedState.Parent = this.siticoneGradientButton1;
+            this.siticoneGradientButton1.CustomImages.Parent = this.siticoneGradientButton1;
+            this.siticoneGradientButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.siticoneGradientButton1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.siticoneGradientButton1.Font = new System.Drawing.Font("Poppins", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneGradientButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneGradientButton1.HoveredState.Parent = this.siticoneGradientButton1;
+            this.siticoneGradientButton1.Location = new System.Drawing.Point(93, 151);
+            this.siticoneGradientButton1.Name = "siticoneGradientButton1";
+            this.siticoneGradientButton1.ShadowDecoration.Parent = this.siticoneGradientButton1;
+            this.siticoneGradientButton1.Size = new System.Drawing.Size(200, 36);
+            this.siticoneGradientButton1.TabIndex = 4;
+            this.siticoneGradientButton1.Text = "Login";
+            this.siticoneGradientButton1.Click += new System.EventHandler(this.siticoneGradientButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.ClientSize = new System.Drawing.Size(389, 275);
+            this.Controls.Add(this.siticoneGradientButton1);
+            this.Controls.Add(this.siticoneLabel2);
+            this.Controls.Add(this.siticoneOSToggleSwith1);
+            this.Controls.Add(this.siticoneMaterialTextBox2);
+            this.Controls.Add(this.siticoneMaterialTextBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+        private Siticone.UI.WinForms.SiticoneElipse siticoneElipse1;
+        private Siticone.UI.WinForms.SiticoneMaterialTextBox siticoneMaterialTextBox1;
+        private Siticone.UI.WinForms.SiticoneOSToggleSwith siticoneOSToggleSwith1;
+        private Siticone.UI.WinForms.SiticoneMaterialTextBox siticoneMaterialTextBox2;
+        private Siticone.UI.WinForms.SiticoneGradientButton siticoneGradientButton1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel2;
+    }
+}
+
---
+++ KeyauthRemember me tut/Form1.cs
@@ -0,0 +1,97 @@
+﻿using KeyAuth;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyauthRemember_me_tut
+{
+    public partial class Form1 : Form
+    {
+
+        public class Credentials
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static api KeyAuthApp = new api(
+             name: "ADD YOURS", // Application Name
+             ownerid: "ADD YOURS", // Owner ID
+             secret: "ADD YOURS", // Application Secret
+             version: "ADD YOURS" // Application Version /*
+            );
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeyAuthApp.init();
+
+
+            string configpath = "Config.json";
+            if (File.Exists(configpath)) 
+                {
+                siticoneOSToggleSwith1.Checked = true;
+                    string json = File.ReadAllText(configpath);
+                Credentials credentials = JsonConvert.DeserializeObject<Credentials>(json);
+                string credentialsString = string.Format("username: {0}, Password: {1}", credentials.Username, credentials.Password);
+                siticoneMaterialTextBox1.Text = credentials.Username;
+                siticoneMaterialTextBox2.Text = credentials.Password;
+                }
+        }
+
+        private void siticoneGradientButton1_Click(object sender, EventArgs e)
+        {
+            if (siticoneOSToggleSwith1.Checked == true)
+            {
+                KeyAuthApp.login(siticoneMaterialTextBox1.Text, siticoneMaterialTextBox2.Text);
+                if (KeyAuthApp.response.success)
+                {
+                    Credentials credentials = new Credentials
+                    {
+                        Username = siticoneMaterialTextBox1.Text,
+                        Password = siticoneMaterialTextBox2.Text
+                    };
+                    string json = JsonConvert.SerializeObject(credentials);
+                    string filePath = "Config.json";
+                    File.WriteAllText(filePath, json);
+                    main main = new main();
+                    main.Show();
+                    this.Hide();
+                }
+            }
+
+            if (siticoneOSToggleSwith1.Checked == false)
+            {
+                KeyAuthApp.login(siticoneMaterialTextBox1.Text, siticoneMaterialTextBox2.Text);
+                if (KeyAuthApp.response.success )
+                {
+                    string filepath = "config.json";
+                    if (File.Exists(filepath))
+                    {
+                        File.Delete(filepath);
+                    }
+                    main main = new main();
+                    main.Show();
+                    this.Hide();
+                }
+            }
+        }
+    }
+}
